753c98d51490c348495bea2550f1d72067598e96
j2noo <wlsdndml213@gmail.com>
2024-04-05 16:38:57 +0900

    chore : swagger 초기 세팅

--- src/main/java/com/example/backendmainserver/HealthTestController.java
+++ src/main/java/com/example/backendmainserver/HealthTestController.java
@@ -1,12 +1,16 @@
 package com.example.backendmainserver;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Health Check API", description = "Health Check API Controller입니다.")
 @RestController
 @RequestMapping("api/health")
 public class HealthTestController {
+    @Operation(summary = "Health Check API", description = "Health Check API입니다.")
     @GetMapping("")
     public String apiHealthTest() {
         return "energiology!";
---
+++ src/main/java/com/example/backendmainserver/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.example.backendmainserver.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Energiology Main Server API 명세서",
+                description = "Energiology Main Server API 명세서",
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+}
