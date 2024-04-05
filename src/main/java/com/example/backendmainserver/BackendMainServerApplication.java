9a1be28489988912618103010db9786bf7593367
youjae <youjae0803@gmail.com>
2024-04-05 13:10:05 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/example/backendmainserver/BackendMainServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendmainserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMainServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMainServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendmainserver/BackendMainServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendmainserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMainServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
