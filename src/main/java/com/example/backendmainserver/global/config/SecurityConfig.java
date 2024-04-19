2ba4763abaddda9c28f454c0083538d314316d22
j2noo <wlsdndml213@gmail.com>
2024-04-19 20:03:41 +0900

    feat : 시큐리티 필터 기본 설정 추가

--- src/main/java/com/example/backendmainserver/global/config/SecurityConfig.java
+++ src/main/java/com/example/backendmainserver/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.backendmainserver.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -23,28 +24,13 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize ->
                         authorize.requestMatchers("/**").permitAll()
                                 .anyRequest().denyAll())
-                .formLogin(AbstractHttpConfigurer::disable); //  폼 기반 시큐리티 로그인 비활성화
-        //
-
-//
-//                .httpBasic(httpBasic -> httpBasic.disable())
-//                .csrf(csrf -> csrf.disable())
-//                .cors(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable) //  폼 기반 시큐리티 로그인 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable);
+//                .cors(Customizer.withDefaults());
 //                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
 //                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .authorizeHttpRequests(request ->
-//                        request.requestMatchers(mvcMatcherBuilder.pattern("/auth/login")).permitAll()
-//                                .requestMatchers(mvcMatcherBuilder.pattern("/css/**")).permitAll()
-//                                .requestMatchers(mvcMatcherBuilder.pattern("/js/**")).permitAll()
-//                                .requestMatchers(mvcMatcherBuilder.pattern("/images/**")).permitAll()
-//                                .requestMatchers(mvcMatcherBuilder.pattern("/error")).permitAll()
-//                                .requestMatchers(mvcMatcherBuilder.pattern("/favicon.ico")).permitAll()
-//                                .requestMatchers(mvcMatcherBuilder.pattern("/swagger-ui/**")).permitAll()
-//                                .requestMatchers(mvcMatcherBuilder.pattern("/swagger-resources/**")).permitAll()
-//                                .requestMatchers(mvcMatcherBuilder.pattern("/v3/api-docs/**")).permitAll()
-////                                .requestMatchers(mvcMatcherBuilder.pattern("/auth/admin/**")).hasAuthority(UserRole.ADMIN.name())
-//                                .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.GET,"/api/**")).permitAll()
-//                                .anyRequest().authenticated());
+//
 //                .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
 
         return http.build();
