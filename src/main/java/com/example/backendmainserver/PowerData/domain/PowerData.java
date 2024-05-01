90700236c6e72d01bf609e6dfa54c1170c9043e4
youjae <youjae0803@gmail.com>
2024-05-01 10:43:23 +0900

    chore: PowerData Builder 추가

--- src/main/java/com/example/backendmainserver/PowerData/domain/PowerData.java
+++ src/main/java/com/example/backendmainserver/PowerData/domain/PowerData.java
@@ -1,5 +1,6 @@
 package com.example.backendmainserver.PowerData.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
 public class PowerData {
     LocalDateTime time;
     Long portId;
