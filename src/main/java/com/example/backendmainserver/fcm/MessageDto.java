b6ce6131933571509737a8501ad5f940e1435b7a
j2noo <wlsdndml213@gmail.com>
2024-05-19 14:59:26 +0900

    feat(#20) : 앱 fcm 환경 구축

---
+++ src/main/java/com/example/backendmainserver/fcm/MessageDto.java
@@ -0,0 +1,13 @@
+package com.example.backendmainserver.fcm;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+public record MessageDto(
+        @Schema(description = "프론트에서 발급받은 디바이스 식별용 fcmToken", required = true)
+        String fcmToken,
+        @Schema(description = "알림 제목")
+        String title,
+        @Schema(description = "알림 내용")
+        String body) {
+}
