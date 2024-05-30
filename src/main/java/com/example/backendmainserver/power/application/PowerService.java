c096258e53e985c88c775ead14cc76e166513db6
j2noo <wlsdndml213@gmail.com>
2024-05-30 16:28:44 +0900

    fix : 1440개만 주도록 변경

--- src/main/java/com/example/backendmainserver/power/application/PowerService.java
+++ src/main/java/com/example/backendmainserver/power/application/PowerService.java
@@ -228,9 +228,10 @@ public class PowerService {
             currentMonthPower.addAll(currentMonthPowerWithPortId);
         }
 
+
+
         Double sumPowerUsage = 0.0;
         Double sumPowerCost = 0.0;
-
 
         for (Power power : currentMonthPower){
             if (power.getPowerUsage() != null) {
@@ -287,6 +288,9 @@ public class PowerService {
         List<Double> powerUsageList = new ArrayList<>() ;
         List<Long> portsId = portService.getPortsId(room);
 
+        //방아이디로 조회
+        List<Double> todayPowerWithRoomId = powerRepository.findTodayPowerWithRoomId(room.getId());
+
         List<Power> currentDailyPower = new ArrayList<>();
         for (Long portId : portsId) {
             List<Power> currentMonthPowerWithPortId = powerRepository.findTodayPowerWithPortId(portId);
@@ -318,7 +322,7 @@ public class PowerService {
 
         return DailyPowerUsageResponse.builder()
                 .powerUsage(sumPowerUsage)
-                .powerUsageDataList(powerUsageList)
+                .powerUsageDataList(todayPowerWithRoomId)
                 .powerSupplierRatio(externalRatio)
                 .build();
     }
@@ -331,11 +335,14 @@ public class PowerService {
         List<Double> powerPredictionList = new ArrayList<>();
         List<Long> portsId = portService.getPortsId(room);
         List<Power> currentDailyPower = new ArrayList<>();
+        List<Double> todayPowerPredictionWithRoomId = powerRepository.findTodayPowerPredictionWithRoomId(room.getId());
 
         for (Long portId : portsId) {
             List<Power> currentMonthPowerWithPortId = powerRepository.findTodayPowerWithPortId(portId);
             currentDailyPower.addAll(currentMonthPowerWithPortId);
         }
+
+
 
 
         double sumPowerPredictionUsage= 0.0;
@@ -349,7 +356,7 @@ public class PowerService {
 
         return DailyPowerPredictionResponse.builder()
                 .powerPredictionUsage(sumPowerPredictionUsage)
-                .powerPredictionDataList(powerPredictionList)
+                .powerPredictionDataList(todayPowerPredictionWithRoomId)
                 .build();
     }
 
